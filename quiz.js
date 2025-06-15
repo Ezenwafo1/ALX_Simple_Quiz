4497fd3bdc1071238167e7851bd42dff24a3ae0c
Anthony Nwambu <aonwambu@gmail.com>
2025-06-15 19:41:25 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 function checkAnswer() {
     const correctAnswer = "4";
 
-    const selectedOption = "document.querySelector('input[name="quiz"]checked:');
+    const selectedOption = document.querySelector('input[name="quiz"]checked:');
 
     if (!selectedOption){
         document.getElementById('feedback').textContent='please select an option before submitting'
