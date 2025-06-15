30455b20b89831841af9a652b3c1e207a2e5bdb9
Anthony Nwambu <aonwambu@gmail.com>
2025-06-15 19:34:50 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,25 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+
+    const selectedOption = document.querySelector ('input[name="quiz"]checked:');
+
+    if (!selectedOption){
+        document.getElementById('feedback').textContent='please select an option before submitting'
+        return;
+     }
+
+     const userAnswer = selectedOption.value;
+
+     if (userAnswer === correctAnswer){
+        document.getElementById('feedback').textContent='correct answer. well done'
+        return;
+     }
+     else{
+        document.getElementById('feedback').textContent='Incorrect. try again';
+
+     }
+     }
+
+     const submitButton = document.getElementById('submit-answer');
+     submitButton.addEventListener('click', checkAnswer);
 
